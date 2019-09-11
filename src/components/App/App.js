ead2b183ecfea370b3aca6fe2e070c9009a7fd59
Gabriella-Kate <gabriellakate04@gmail.com>
2019-09-11 22:09:10 +0200

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Quote from '../Quote/Quote.js';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Quote/>
+    </div>
+  );
+}
+
+export default App;
